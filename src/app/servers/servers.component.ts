3ef8898f17a9d03291da04e39fab026d5de946f4
daisymae <daisymae@orcadiafarm.com>
2018-04-29 15:16:15 -0500

    More route examples
    Another example of using navigate along with relativeTo and
    ActivatedRoute. This will break the application, so code is there, but
    commented out.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,24 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    console.log('reload page');
+    /*
+    can use '/servers' here, but also can use relative 'servers'
+    if using relative, need to tell Angular what the path is relativeTo
+    the default is '/' so relative or absolute are the same if they are
+    relativeTo root, which is the case here, but want to show how the code
+    works if it were not the case
+    This breaks the code here because will try to go to 'servers/servers'
+    */
+    // this.router.navigate(['servers'], {relativeTo: this.route});
+  }
 }
